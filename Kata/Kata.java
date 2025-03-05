4b4fea1b8379ae79a6f434b897b006d75b0d69f1
Arturo802 <eteandres01@gmail.com>
2025-03-05 02:14:20 +0100

    Segundo commit

--- Kata/Kata.java
+++ Kata/Kata.java
@@ -1,6 +1,20 @@
 public class Kata {
-    public static int sum(int[] numbers){
+    public static int sum(int[] numeros){
     /* Para guardar los valores mínimos y máximos del array. */
+        
+    int min = numeros[0] , max = numeros[0];
+    int suma = 0;
+
+    for(int numero: numeros){
+        if(min > numero)
+            min = numero;
+        if (max < numero) 
+            max = numero;
+
+            suma += numero;
     }
-    return 0;
+    return (suma - min) - max;
+
+    }
+    
 }
